513790690fd91c014a82b3ebf56ef181c400e249
Iqra327 <iqrarasheed219@gmail.com>
2024-05-29 17:09:50 +0500

    Initial commit

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,157 +1,103 @@
 import { GetFormStats, GetForms } from "@/actions/form";
-import {LuView} from "react-icons/lu";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { ReactNode, Suspense } from "react";
+import { LuView } from "react-icons/lu";
 import { FaWpforms } from "react-icons/fa";
-import { HiCursorClick } from "react-icons/hi"
+import { HiCursorClick } from "react-icons/hi";
 import { TbArrowBounce } from "react-icons/tb";
 import { Separator } from "@/components/ui/separator";
 import CreateFormBtn from "@/components/CreateFormBtn";
-import { formSchema } from "@/schemas/form";
+import { Form } from "@prisma/client";
 import { Badge } from "@/components/ui/badge";
 import { formatDistance } from "date-fns";
-import { Form } from "@prisma/client";
-import { BiRightArrowAlt } from "react-icons/bi";
-import { FaEdit } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { BiRightArrowAlt } from "react-icons/bi";
+import { FaEdit } from "react-icons/fa";
 
-
-export default function Home(){
+export default function Home() {
   return (
     <div className="container pt-4">
       <Suspense fallback={<StatsCards loading={true} />}>
-      <CardStatsWrapper /> {/*all cards*/}
+        <CardStatsWrapper />
       </Suspense>
       <Separator className="my-6" />
       <h2 className="text-4xl font-bold col-span-2">Your forms</h2>
       <Separator className="my-6" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid gric-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <CreateFormBtn />
-        <Suspense 
-        fallback={[1, 2, 3, 4].map((el) => (
-          <FormCardSkeleton key={el} />
-        ))}
+        <Suspense
+          fallback={[1, 2, 3, 4].map((el) => (
+            <FormCardSkeleton key={el} />
+          ))}
         >
           <FormCards />
         </Suspense>
       </div>
-     
     </div>
   );
 }
 
-async function CardStatsWrapper(){
+async function CardStatsWrapper() {
   const stats = await GetFormStats();
   return <StatsCards loading={false} data={stats} />;
 }
 
-interface StatsCardProps{
+interface StatsCardProps {
   data?: Awaited<ReturnType<typeof GetFormStats>>;
   loading: boolean;
 }
 
-// function StatsCards(props: StatsCardProps){
-//   const {data, loading } = props;
-  
-//   return (
-//    <div className ="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-    
-//     {/*card 1 */}
-//     <StatsCard
-//     title="Total visits"
-//     icon={<LuView className="text-blue-600" /> }
-//     helperText="All time form visits"
-//     value={data?.visits.toLocaleString() || ""}
-//     loading={loading}
-//     className="shadow-md shadow-blue-600"
-//      />
-
-//      {/*card 2 */}
-//      <StatsCard
-//     title="Total submissions"
-//     icon={<FaWpforms className="text-yellow-600" /> }
-//     helperText="All time form submissions"
-//     value={data?.visits.toLocaleString() || ""}
-//     loading={loading}
-//     className="shadow-md shadow-yellow-600"
-//      />
-
-//      {/*card 3 */}
-//      <StatsCard
-//     title="Submission Rate"
-//     icon={<HiCursorClick className="text-green-600" /> }
-//     helperText="Visits that result in form submission"
-//     value={data?.visits.toLocaleString() + "%" || ""}
-//     loading={loading}
-//     className="shadow-md shadow-green-600"
-//      />
-
-//      {/*card 4 */}
-//      <StatsCard
-//     title="Bounce Rate"
-//     icon={<TbArrowBounce className="text-red-600" /> }
-//     helperText="Visits that leave without interacting"
-//     value={data?.visits.toLocaleString() + "%"|| ""}
-//     loading={loading}
-//     className="shadow-md shadow-red-600"
-//      />
-//   </div>
-//   );
-// }
-
-function StatsCards(props: StatsCardProps){
-  const {data, loading } = props;
+function StatsCards(props: StatsCardProps) {
+  const { data, loading } = props;
 
   return (
-   <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-    
-    {/*card 1 */}
-    <StatsCard
-      title="Total visits"
-      icon={<LuView className="text-blue-600" /> }
-      helperText="All time form visits"
-      value={data?.visits.toLocaleString() || ""}
-      loading={loading}
-      className="shadow-md shadow-blue-600"
-    />
+    <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      {/*card 1 */}
+<StatsCard
+title="Total visits"
+icon={<LuView className="text-blue-600" /> }
+helperText="All time form visits"
+value={data?.visits.toLocaleString() || ""}
+loading={loading}
+className="shadow-md shadow-blue-600"
+/>
 
-    {/*card 2 */}
-    <StatsCard
-      title="Total submissions"
-      icon={<FaWpforms className="text-yellow-600" /> }
-      helperText="All time form submissions"
-      value={data?.submissions.toLocaleString() || ""}
-      loading={loading}
-      className="shadow-md shadow-yellow-600"
-    />
+{/*card 2 */}
+<StatsCard
+title="Total submissions"
+icon={<FaWpforms className="text-yellow-600" /> }
+helperText="All time form submissions"
+value={data?.submissions.toLocaleString() || ""}
+loading={loading}
+className="shadow-md shadow-yellow-600"
+/>
 
-    {/*card 3 */}
-    <StatsCard
-      title="Submission Rate"
-      icon={<HiCursorClick className="text-green-600" /> }
-      helperText="Visits that result in form submission"
-      value={(data?.submissionRate.toFixed(2) + "%" || "")}
-      loading={loading}
-      className="shadow-md shadow-green-600"
-    />
+{/*card 3 */}
+<StatsCard
+title="Submission Rate"
+icon={<HiCursorClick className="text-green-600" /> }
+helperText="Visits that result in form submission"
+value={(data?.submissionRate.toFixed(2) + "%" || "")}
+loading={loading}
+className="shadow-md shadow-green-600"
+/>
 
-    {/*card 4 */}
-    <StatsCard
-      title="Bounce Rate"
-      icon={<TbArrowBounce className="text-red-600" /> }
-      helperText="Visits that leave without interacting"
-      value={(data?.bounceRate.toFixed(2) + "%" || "")}
-      loading={loading}
-      className="shadow-md shadow-red-600"
-    />
-  </div>
+{/*card 4 */}
+<StatsCard
+title="Bounce Rate"
+icon={<TbArrowBounce className="text-red-600" /> }
+helperText="Visits that leave without interacting"
+value={(data?.bounceRate.toFixed(2) + "%" || "")}
+loading={loading}
+className="shadow-md shadow-red-600"
+/>
+    </div>
   );
 }
 
-
- export function StatsCard({ 
+export function StatsCard({
   title,
   value,
   icon,
@@ -167,85 +113,84 @@ function StatsCards(props: StatsCardProps){
   icon: ReactNode;
 }) {
   return (
-  <Card className={className}>
-    <CardHeader className="flex flex-row items-center justify-between pb-2">
-      <CardTitle className="text-sm font-medium text-muted-foreground">{title}</CardTitle>
-      {icon}
-    </CardHeader>
-    <CardContent>
-      <div className="text-2xl font-bold">
-        {loading && (
-          <Skeleton>
-            <span className="opacity-0">0</span>
-          </Skeleton>
-        )}
-        {!loading && value}
-      </div>
-      <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
-    </CardContent>
-  </Card>
+    <Card className={className}>
+      <CardHeader className="flex flex-row items-center justify-between pb-2">
+        <CardTitle className="text-sm font-medium text-muted-foreground">{title}</CardTitle>
+        {icon}
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">
+          {loading && (
+            <Skeleton>
+              <span className="opacity-0">0</span>
+            </Skeleton>
+          )}
+          {!loading && value}
+        </div>
+        <p className="text-xs text-muted-foreground pt-1">{helperText}</p>
+      </CardContent>
+    </Card>
   );
 }
 
-function FormCardSkeleton(){
-  return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full"/>
+function FormCardSkeleton() {
+  return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full" />;
 }
 
-async function FormCards(){
+async function FormCards() {
   const forms = await GetForms();
   return (
     <>
-      {forms.map((form) =>(
-        <FormCard key={form.id} form={form}/>
+      {forms.map((form) => (
+        <FormCard key={form.id} form={form} />
       ))}
     </>
-  ); 
+  );
 }
 
-function FormCard({ form }: {form: Form }){
-  return ( <Card>
-    <CardHeader>
-      <CardTitle className="flex items-center gap-2 justify-between">
-        <span className="truncate font-bold">
-          {form.name}
-        </span>
-        {form.published && <Badge>Published</Badge>}
-        {!form.published && <Badge variant={"destructive"}>Draft</Badge>}
-      </CardTitle>
-      <CardDescription>
-        {formatDistance(form.createdAt, new Date(), {
-          addSuffix: true,
-        })}
-        {form.published && (
-          <span className="flex items-center gap-2">
-            <LuView className="text-muted-foreground"/>
-            <span>{form.visits.toLocaleString()}</span>
-            <FaWpforms className="text-muted-foreground" />
-            <span>{form.submissions.toLocaleString()}</span>
-          </span>
-        )}
-      </CardDescription>
-    </CardHeader>
+function FormCard({ form }: { form: Form }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2 justify-between">
+          <span className="truncate font-bold">{form.name}</span>
+          {form.published && <Badge>Published</Badge>}
+          {!form.published && <Badge variant={"destructive"}>Draft</Badge>}
+        </CardTitle>
+        <CardDescription className="flex items-center justify-between text-muted-foreground text-sm">
+          {formatDistance(form.createdAt, new Date(), {
+            addSuffix: true,
+          })}
+          {form.published && (
+            <span className="flex items-center gap-2">
+              <LuView className="text-muted-foreground" />
+              <span>{form.visits.toLocaleString()}</span>
+              <FaWpforms className="text-muted-foreground" />
+              <span>{form.submissions.toLocaleString()}</span>
+            </span>
+          )}
+        </CardDescription>
+      </CardHeader>
       <CardContent className="h-[20px] truncate text-sm text-muted-foreground">
-        {form.description || "No description"}              
+        {form.description || "No description"}
       </CardContent>
-      <CardFooter >
+      <CardFooter>
         {form.published && (
           <Button asChild className="w-full mt-2 text-md gap-4">
-            <Link href={'/forms/${form.id}'}>
+            <Link href={`/forms/${form.id}`}>
               View submissions <BiRightArrowAlt />
             </Link>
           </Button>
         )}
         {!form.published && (
-          <Button asChild 
-          variant={"secondary"} className="w-full mt-2 text-md gap-4">
+          <Button asChild variant={"secondary"} className="w-full mt-2 text-md gap-4">
             <Link href={`/builder/${form.id}`}>
-              Edit form <FaEdit /> 
+              Edit form <FaEdit />
             </Link>
           </Button>
         )}
-    </CardFooter>
-  </Card>
+      </CardFooter>
+    </Card>
   );
 }
+
